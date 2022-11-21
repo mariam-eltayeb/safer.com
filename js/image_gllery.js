e64666b74da518be17de0ef62d3d159c98e35816
mariam-eltayeb <mariam.eltayeb@outlook.com>
2022-11-21 23:21:05 +0200

    arrows , hoverimage in hotel details

--- js/image_gllery.js
+++ js/image_gllery.js
@@ -19,3 +19,37 @@ function changeImageOnClick(event) {
     mainImage.src = targetElement.getAttribute("src");
   }
 }
+//  sider for image 
+var images = [ "tour-details-1.webp","tour-details-2.webp","tour-details-3.webp" ];
+var slider = document.getElementById('mainImage');
+var prevoiusButton = document.getElementById('previous');
+var nextButton = document.getElementById('next');
+var imageIndex = 0;
+
+nextButton.addEventListener('click' , function (){
+    imageIndex++;
+    link = "./images/tour-details/" + images[imageIndex];
+    slider.setAttribute('src' , link);
+
+    if(imageIndex == images.length - 1 ){
+        nextButton.setAttribute('disabled','');
+        prevoiusButton.removeAttribute('disabled','')
+    }
+    else{
+        prevoiusButton.removeAttribute('disabled','')
+    }
+})
+
+prevoiusButton.addEventListener('click' , function (){
+    imageIndex--;
+    link = "./images/tour-details/" + images[imageIndex];
+    slider.setAttribute('src' , link);
+
+    if(imageIndex == 0 ){
+        prevoiusButton.setAttribute('disabled','');
+        nextButton.removeAttribute('disabled','')
+    }
+    else{
+        nextButton.removeAttribute('disabled','')
+    }
+})
