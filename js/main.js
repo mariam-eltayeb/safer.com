59f8600e54dbb96908734f47c048dda141a68c50
mariam-eltayeb <mariam.eltayeb@outlook.com>
2022-11-28 23:16:33 +0200

    linking trasnfer and visa in all pages , logout modal in my profile page , name of user drop down in my profile

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ function openFilter(){
     filterButton.classList.toggle ("background");
 }
 
+// profile page dropdown toggle
+
+var logout_dropdown = document.getElementById('logout_dropdown');
+    function opendropdown(){
+        logout_dropdown.classList.toggle("show");
+    }
+    
